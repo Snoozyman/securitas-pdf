6793cdd9f0e49786b2292cd075a75b57ebd21813
Snoozyman <barraculada@gmail.com>
2022-03-07 01:17:46 +0200

    Port

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 80
+const port = 8080
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const bodyParser = require('body-parser');
