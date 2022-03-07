8a34e9ca56f43efffe85b278f0b415f889375d08
Snoozyman <barraculada@gmail.com>
2022-03-08 00:10:21 +0200

    Fixes

--- dist/index.js
+++ dist/index.js
@@ -86,42 +86,121 @@ app.post('/tyot', async (req, res) => {
       }).then(data => {
         data.pages; // the number of pages
 
-        let moi = parse(data.text.toString());
-        let {
-          value
-        } = ics.createEvents(moi);
-        (0, _fs.writeFileSync)('public/events.ics', value);
-        res.sendFile('events.ics', {
-          root: 'public'
+        let events = [];
+        let lista = parse(data.text.toString());
+        lista.forEach(t => {
+          events.push({
+            start: [t.start.getFullYear(), t.start.getMonth() + 1, t.start.getDate(), t.start.getHours(), t.start.getMinutes()],
+            //startInputType: 'utc',
+            startOutputType: 'local',
+            end: [t.end.getFullYear(), t.end.getMonth() + 1, t.end.getDate(), t.end.getHours(), t.end.getMinutes()],
+            //endInputType: 'utc',
+            endOutputType: 'local',
+            title: t.Summary,
+            location: t.Address
+          });
         });
-      }); //send response
+        const {
+          error,
+          value
+        } = ics.createEvents(events);
+        if (error) res.send(error);
+        (0, _fs.writeFileSync)('public/events.ics', value); // res.sendFile('events.ics', {root: 'public'})
 
-      /*res.send({
-          status: true,
-          message: 'File is uploaded',
-          data: {
-              name: lista.name,
-              mimetype: lista.mimetype,
-              size: lista.size
-          }
-      });*/
-
+        res.download('public/events.ics', 'tyolista-' + moment().format("YYYYMMDD"));
+      });
     }
   } catch (err) {
     res.status(500).send(err);
   }
 });
-app.get('/pdf', (req, res) => {});
+app.get('/pdf', (req, res) => {
+  let p = `
+Hnro36264Securitas Oy
+NimiMikko Tuomikoski
+Työvuorolista
+4.3.2022 - 3.4.2022
+PVM Alkaa/PäättyyLähtöpaikka/yksikköKuvausTunnitViikkotunnit
+pe18.3 18:00 - 06:00 Elimäenkatu 28Helsinki 60512:00
+la19.3 19:00 - 06:00 Elimäenkatu 28Helsinki 60511:00
+su20.3 19:00 - 06:00 Elimäenkatu 28Helsinki 605 11:0028:00
+ke23.3 18:00 - 06:00 Elimäenkatu 28Helsinki 60512:00
+to24.3 18:00 - 06:00 Elimäenkatu 28Helsinki 605 12:0030:00
+        ma28.3 18:00 - 06:00 Elimäenkatu 28Helsinki 60512:00
+        ti29.3 18:00 - 06:00 Elimäenkatu 28Helsinki 60512:00
+        pe1.4 18:00 - 06:00 Elimäenkatu 28Helsinki 60512:00
+        la2.4 19:00 - 06:00 Elimäenkatu 28Helsinki 60511:00
+        su3.4 19:00 - 06:00 Elimäenkatu 28Helsinki 605 5:0052:00
+        Tulostettu: 27.2.2022 20:54:47 
+        Jakson tunnit 110:00
+        yhteensä:`;
+  let events = [];
+  let lista = parse(p);
+  lista.forEach(t => {
+    events.push({
+      start: [t.start.getFullYear(), t.start.getMonth() + 1, t.start.getDate(), t.start.getHours(), t.start.getMinutes()],
+      //startInputType: 'utc',
+      startOutputType: 'local',
+      end: [t.end.getFullYear(), t.end.getMonth() + 1, t.end.getDate(), t.end.getHours(), t.end.getMinutes()],
+      //endInputType: 'utc',
+      endOutputType: 'local',
+      title: t.Summary,
+      location: t.Address
+    });
+  });
+  const {
+    error,
+    value
+  } = ics.createEvents(events);
+  if (error) res.send(error);
+  res.send(value);
+});
 
 class tyovuoro {
   constructor(Weekday, Date, StartTime, EndTime, Address, Summary, Length) {
     this.Weekday = Weekday;
-    this.Date = Date;
-    this.StartTime = StartTime;
-    this.EndTime = EndTime;
+    this.Date = {
+      day: parseInt(Date.split('.')[0]),
+      month: parseInt(Date.split('.')[1])
+    };
+    this.StartTime = {
+      hours: parseInt(StartTime.split(':')[0]),
+      mins: parseInt(StartTime.split(':')[1])
+    };
+    this.EndTime = {
+      hours: parseInt(EndTime.split(':')[0]),
+      mins: parseInt(EndTime.split(':')[1])
+    };
     this.Address = Address;
     this.Summary = Summary;
-    this.Length = Length;
+    this.Length = {
+      hours: parseInt(Length.split(':')[0]),
+      mins: parseInt(Length.split(':')[1])
+    };
+    this.start;
+    this.end;
+  }
+
+  toISOString() {
+    let start = new Date();
+    let end = new Date();
+    let year = start.getFullYear();
+    start.setFullYear(year, this.Date.month - 1, this.Date.day);
+    start.setHours(this.StartTime.hours, this.StartTime.mins, 0, 0);
+    this.start = start;
+    let enddate = this.Date.day;
+
+    if (this.isNextDay()) {
+      enddate += 1;
+    }
+
+    end.setFullYear(year, this.Date.month - 1, enddate);
+    end.setHours(this.EndTime.hours, this.EndTime.mins, 0, 0);
+    this.end = end;
+  }
+
+  isNextDay() {
+    if (this.StartTime.hours > this.EndTime.hours & this.EndTime.hours >= 0) return true;else return false;
   }
 
 }
@@ -131,39 +210,19 @@ app.listen(port, () => {
 });
 
 function parse(text) {
-  console.log(text);
+  // console.log(text)
+  let lista = [];
   const alku = /Nimi(.+\w)\n(.+)\n(\d+.\d+.\d+)\s-\s(\d\d?.\d\d?.\d+)/g;
-  const helsinki = /([a-z]{2})(\d+.\d+)\s(\d+:\d+)\s-\s(\d+:\d+)\s([A-zäö]+\s\d+)([A-z]+\s\d{1,3})\s?(\d+:\d\d)(\d+:\d+)?\n?/g;
+  const helsinki = /([a-z]{2})(\d+\.\d+)\s(\d+\:\d+)\s\-\s(\d+:\d+)\s([A-zäö]+\s\d+)([A-z]+\s\d{1,3})\s?(\d+\:\d\d)(\d+:\d+)?/gm;
   const pori = /([a-z]{2})(\d+.\d+)\s(\d+:\d+)?\s-\s(\d+:\d+)\s(.*)(\d{3}\s[A-zöä]+)\s?(\d+:\d\d)/g;
-  let matches;
-  let events = [];
-  if (helsinki.test(text)) matches = text.matchAll(helsinki);else if (pori.test(text)) matches = text.matchAll(pori);else return "Error";
-  let listobj = [];
+  const matches = text.matchAll(helsinki);
 
   for (const match of matches) {
-    listobj.push(new tyovuoro(match[1], match[2], match[3], match[4], match[5], match[6], match[7]));
-    let start = new Date();
-    let end = new Date();
-    let date = match[2].split('.');
-    let time = match[3].split(':');
-    let etime = match[4].split(':');
-    let duration = match[7].split(':');
-    start.setHours(parseInt(time[0]), parseInt(time[1]));
-    start.setFullYear(2022, parseInt(date[1]) - 1, parseInt(date[0]));
-    end.setHours(parseInt());
-    if (parseInt(etime[0] > 0)) start.setFullYear(2022, parseInt(date[1]) - 1, parseInt(date[0]) + 1);
-    if (parseInt(duration[0]) < 6) duration;
-    events.push({
-      title: match[6],
-      location: match[5],
-      start: [start.getFullYear(), start.getMonth() + 1, start.getDate(), start.getHours(), start.getMinutes()],
-      duration: {
-        hours: parseInt(duration[0]),
-        minutes: parseInt(duration[1])
-      }
-    });
+    lista.push(new tyovuoro(match[1], match[2], match[3], match[4], match[5], match[6], match[7]));
   }
 
-  console.log(events);
-  return events;
+  lista.forEach(t => {
+    t.toISOString();
+  });
+  return lista;
 }
